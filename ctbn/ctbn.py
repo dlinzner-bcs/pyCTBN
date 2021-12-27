1ceefd28f528701f8984b50cde85bac3ee5022ec
Dominik Linzner <44400328+dlinzner-bcs@users.noreply.github.com>
2021-12-27 18:34:11 +0100

    - progress on sampler

--- ctbn/ctbn.py
+++ ctbn/ctbn.py
@@ -1,12 +1,18 @@
 import numpy as np
 import itertools
-from typing import List, NewType
+from typing import List, NewType, Optional
 from numpy.core.fromnumeric import shape
 
 
 State = NewType('State', int)
 States = NewType('States', List[State])
-Transition = NewType('Transition', tuple[States, float])
+
+
+class Transition:
+    def __init__(self, s0: State, s1: State, tau: float) -> None:
+        self._s_init = s0
+        self._s_final = s1
+        self._exit_time = tau
 
 
 class IM:
@@ -32,37 +38,93 @@ class IM:
     def random_from_dim(self, dim: int, alpha: float, beta: float) -> 'IM':
         return IM(matrix=np.random.gamma(shape=alpha, scale=1/beta, size=(dim, dim)))
 
+    def __repr__(self) -> str:
+        return np.array2string(self._im)
+
 
 class Node:
-    def __init__(self, states: States, parents: List['Node'], children: List['Node']) -> 'Node':
+    def __init__(self, state: State, states: States, parents: List['Node'], children: List['Node']) -> 'Node':
         self._states = states
         self._parents = parents
         self._children = children
         self._cims = None
+        self._state = state
+        self._cim: Optional[IM]
+        self._cim = None
 
     @ property
     def cims(self):
         return self._cims
 
+    @property
+    def state(self):
+        return self._state
+
     def generate_empty_cims(self):
         cims = dict()
-        for states in itertools.product([p._states for p in self._parents]):
-            for state in states:
-                dim = int(len(self._states))
-                state_map = tuple(state)
-                cim = IM.empty_from_dim(dim)
-                cims[state_map] = cim
+        if isinstance(self._parents, type(None)):
+            dim = int(len(self._states))
+            cim = IM.empty_from_dim(dim)
+            cims[None] = cim
+        else:
+            for states in itertools.product([p._states for p in self._parents]):
+                for state in states:
+                    dim = int(len(self._states))
+                    state_map = tuple(state)
+                    cim = IM.empty_from_dim(dim)
+                    cims[state_map] = cim
         self._cims = cims
 
     def generate_random_cims(self, alpha: float, beta: float):
         cims = dict()
-        for states in itertools.product([p._states for p in self._parents]):
-            for state in states:
-                dim = int(len(self._states))
-                state_map = tuple(state)
-                cim = IM.random_from_dim(dim, alpha, beta)
-                cims[state_map] = cim
+        if self._parents is None:
+            dim = int(len(self._states))
+            cim = IM.random_from_dim(dim, alpha, beta)
+            cims[None] = cim
+        else:
+            for states in itertools.product([p._states for p in self._parents]):
+                for state in states:
+                    dim = int(len(self._states))
+                    state_map = tuple(state)
+                    cim = IM.random_from_dim(dim, alpha, beta)
+                    cims[state_map] = cim
         self._cims = cims
+
+    @property
+    def cim(self) -> IM:
+        if self._parents is None:
+            return self._cims[None]
+        else:
+            parent_state = tuple([p._states for p in self._parents][0])
+            return self._cims[parent_state]
+
+    @property
+    def exit_rate(self) -> Optional[float]:
+        if self.cim is None:
+            return None
+        else:
+            return -self.cim._im[self._state, self._state]
+
+    @property
+    def transition_rates(self) -> Optional[np.numarray]:
+        if self.cim is None:
+            return None
+        else:
+            transition_rates = self.cim._im[self._state, :]
+            transition_rates[self._state] = 0
+            return transition_rates
+
+    def next_state(self):
+        transition_rates = self.transition_rates
+        if transition_rates is None:
+            pass
+        else:
+            cum_prob = np.cumsum(self.transition_rates) / \
+                np.sum(self.transition_rates)
+            self._state = State(np.argmax(np.random.uniform() <= cum_prob))
+
+    def exit_time(self):
+        return np.random.exponential(self.exit_rate)
 
 
 class Graph:
@@ -78,6 +140,27 @@ class CTBN(Graph):
     def __init__(self, nodes: List[Node], alpha: float, beta: float):
         super().__init__(nodes)
         [n.generate_random_cims(alpha, beta) for n in self._nodes]
+        [n.set_cim() for n in self._nodes]
 
-    def sample_transition(self, s0: States) -> Transition:
-        pass
+    def active_node(self) -> 'Node':
+        rates = [0 if np.exit_rate is None else n.exit_rate for n in self._nodes]
+        cum_prob = np.cumsum(rates) / \
+            np.sum(self.transition_rates)
+        return self._nodes[np.argmax(np.random.uniform() <= cum_prob)]
+
+    def transition(self) -> Transition:
+        node = self.active_node()
+        tau = node.exit_time()
+        s_0 = node.state
+        node.next_state()
+        s_1 = node.state
+        return Transition(s_0, s_1, tau)
+
+
+class Trajectory:
+    def __init__(self) -> None:
+        self._transitions: List[Transition]
+        self._transitions = list()
+
+    def append(self):
+        self._transitions.append(Transition)
--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from ctbn.ctbn import Node, CTBN, State, States
 
 if __name__ == "__main__":
-    states = States(list([State(0), State(1)]))
-    node_A = Node(states, parents=None, children=None)
-    node_B = Node(states, parents=list([node_A]), children=None)
+    states = States(list([State(0), State(1), State(3)]))
+    node_A = Node(state=State(0), states=states, parents=None, children=None)
+    node_B = Node(state=State(1), states=states,
+                  parents=list([node_A]), children=None)
+    node_A.generate_random_cims(1.0, 1.0)
     node_B.generate_random_cims(1.0, 1.0)
-    2
+    print(node_B.cim)
+    print(node_B.exit_rate)
+    print(node_A.cim)
+    print(node_A.exit_rate)
+    print(node_B.transition_rates)
+    print(node_B.transition_rates)
+    for k in range(0, 10):
+        print(node_B.transition_rates)
+        node_B.next_state()
+        print(node_B._state)
