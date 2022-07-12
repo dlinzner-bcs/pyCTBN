ff282f765a6929bb950c5e40736db3b4c4db6fbf
dlinzner-bcs <dlinzner90@gmail.com>
2022-07-12 18:18:26 +0200

    - implement structure learning

--- ctbn/learner.py
+++ ctbn/learner.py
@@ -1,8 +1,10 @@
 from copy import deepcopy
 from ctbn.ctbn_model import State, States, Transition, CTBN, CTBNNode, IM
 from ctbn.graph import Node
-from ctbn.types import Transition, ActiveTransition
+from ctbn.types import Trajectory, Transition, ActiveTransition
 from typing import List
+from itertools import chain, combinations
+from scipy.special import gammaln
 import numpy as np
 import unittest
 
@@ -24,11 +26,11 @@ class CTBNLearnerNode(CTBNNode):
 
         self._transition_stats = transition_stats
         self._exit_time_stats = exit_time_stats
-        self._cims = None
+        #self._cims = None
 
     @classmethod
-    def from_ctbn_node(self, node: CTBNNode, alpha=1.0, beta=1.0):
-        return CTBNLearnerNode(node.state, node.states, node.parents)
+    def from_ctbn_node(self, node: CTBNNode):
+        return CTBNLearnerNode(node.state, node.states, node.parents,node._alpha,node._beta)
 
     def reset_stats(self):
         transition_stats = dict()
@@ -45,6 +47,28 @@ class CTBNLearnerNode(CTBNNode):
 
         self._transition_stats = transition_stats
         self._exit_time_stats = exit_time_stats
+
+    def update_stats(self, transition: Transition):
+        if self.nid == transition._node_id:
+            node = self
+            if node._parents is None:
+                p_state = None
+            else:
+                p_state = tuple([transition._s_init[n.nid]
+                                for n in node._parents])
+            s0 = transition._s_init[node.nid]
+            s1 = transition._s_final[node.nid]
+            t_stat = node._transition_stats[p_state]
+            t_stat[s0, s1] += 1
+        else:
+            s0 = transition._s_init[self.nid]
+            if self._parents is None:
+                p_state = None
+            else:
+                p_state = tuple([transition._s_init[n_p.nid]
+                                for n_p in self._parents])
+            e_stat =  self._exit_time_stats[p_state]
+            e_stat[s0] += transition._exit_time
 
     def estimate_cims(self):
         cims = dict()
@@ -85,6 +109,17 @@ class CTBNLearnerNode(CTBNNode):
                             e_stat[s]*self._cims[key].im[s, s]
         return llh
 
+    def structure_score(self):
+        llh = 0
+        for key in self._transition_stats.keys():
+            t_stat = self._transition_stats[key]
+            e_stat = self._exit_time_stats[key]
+            for s in self._states:
+                for s_ in self._states:
+                    if s != s_:
+                        llh += gammaln(t_stat[s, s_]) - \
+                            t_stat[s, s_]*np.log(e_stat[s])
+        return llh
 
 class CTBNLearner(CTBN):
     def __init__(self, nodes: List[CTBNLearnerNode]):
@@ -196,6 +231,30 @@ class CTBNLearner(CTBN):
             simulator.average_stats_active(
                 self.intervention(intervention).transition(), num_samples)
         return simulator
+
+    def score_parents_of_node(self,node: CTBNLearnerNode,data: Trajectory):
+        ctbn_learner_ = deepcopy(self)
+        node_ = ctbn_learner_.node_by_id(node.nid)
+        scores ={}
+        for parent_candidate in self.node_powerset(3):
+            ctbn_learner_.reset_stats()
+            node_.set_parents(parent_candidate)
+            node_.generate_random_cims(node_._alpha,node_._beta)
+            node_.reset_stats()
+            ctbn_learner_ = CTBNLearner(ctbn_learner_.nodes)
+            for trans in data:
+                node_.update_stats(trans)
+                #ctbn_learner_.update_stats(trans)
+            ctbn_learner_.estimate_cims()
+            scores[tuple([n.nid for n in parent_candidate])]  = node_.structure_score()
+        return scores
+    
+    def node_powerset(self,k):
+        "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
+        s = list(self.nodes)
+        return chain.from_iterable(combinations(s, r) for r in range(0,k))
+
+
 
 
 class TestLearner(unittest.TestCase):
